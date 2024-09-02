f3f476a0c33f64022bda6cab78e25bd3cd0a424f
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-01 21:37:41 -0600

    Creacion de la Entidad de Prioridades

---
+++ app/src/main/java/edu/ucne/prioridades/data/local/entities/PrioridadEntity.kt
@@ -0,0 +1,14 @@
+package edu.ucne.prioridades.data.local.entities
+
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Prioridades")
+data class PrioridadEntity(
+    @PrimaryKey
+    val prioridadId: Int? = null,
+    val descripcion: String = "",
+    val diasCompromiso: Int
+)
