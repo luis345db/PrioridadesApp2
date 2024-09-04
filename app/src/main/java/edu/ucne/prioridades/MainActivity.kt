38fb2c4ebf4b40cf255833435b5c3d5df0b934fb
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-03 20:32:40 -0600

    Creacion del registro

--- app/src/main/java/edu/ucne/prioridades/MainActivity.kt
+++ app/src/main/java/edu/ucne/prioridades/MainActivity.kt
@@ -5,23 +5,40 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.room.Room
 import edu.ucne.prioridades.data.local.database.PrioridadDB
+import edu.ucne.prioridades.data.local.entities.PrioridadEntity
 import edu.ucne.prioridades.ui.theme.PrioridadesTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -33,8 +50,9 @@ class MainActivity : ComponentActivity() {
 
         prioridadDb = Room.databaseBuilder(
             applicationContext,
-            PrioridadDB::class.java,"Prioridad.db"
+            PrioridadDB::class.java, "Prioridad.db"
         ).fallbackToDestructiveMigration().build()
+
         setContent {
             PrioridadesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -46,51 +64,135 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun PrioridadScreen(name: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-        )       {
-                    OutlinedTextField(
-                        modifier =Modifier.fillMaxWidth(),
-                        label = {
-                Text(text = "Descripcion")
-            },
-            value = "",
-            onValueChange = {}
 
-        )
+    @Composable
+    fun PrioridadScreen(name: String, modifier: Modifier = Modifier) {
 
-        OutlinedTextField(
-            modifier =Modifier.fillMaxWidth(),
-
-            label = {
-                Text(text = "Dias Compromiso")
-                    },
-            value = "",
-            onValueChange = {}
-        )
-
-        OutlinedButton(
-            modifier = Modifier.fillMaxWidth(),
-            onClick = {  }
-        ) {
-            Icon(Icons.Default.Add, contentDescription ="Guardar")
-            Text(text = "Guardar")
-            
+        var descripcion by remember { mutableStateOf("") }
+        var diasCompromiso by remember { mutableStateOf("") }
+        var errorMessage: String? by remember {
+            mutableStateOf(null)
         }
+        val scope = rememberCoroutineScope()
+
+
+        Scaffold { innerPadding ->
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(innerPadding)
+                    .padding(8.dp)
+            ) {
+                ElevatedCard(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Prioridades",
+                            style = MaterialTheme.typography.displaySmall,
+                            fontWeight = FontWeight.Bold
+                        )
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            label = {
+                                Text(text = "Descripcion")
+                            },
+                            value = descripcion,
+                            onValueChange = {
+                                descripcion = it
+                                errorMessage = null
+                            },
+
+
+                            )
+
+                        OutlinedTextField(
+
+                            modifier = Modifier.fillMaxWidth(),
+                            label = {
+                                Text(text = "Dias de compromiso")
+                            },
+                            value = diasCompromiso,
+                            onValueChange = {
+                                diasCompromiso = it
+                                errorMessage = null
+                            },
+                            /*keyboardActions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number
+                )*/
+                        )
+
+                        Spacer(
+                            modifier = Modifier.height(10.dp)
+                        )
+                        errorMessage?.let {
+                            Text(
+                                text = it,
+                                color = Color.Red
+                            )
+                        }
+
+                        OutlinedButton(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                if (descripcion.isBlank())
+                                    errorMessage = "La descripción no puede estar vacía"
+                                else if (diasCompromiso.isBlank())
+                                    errorMessage = "Días de compromiso no puede ir vacío"
+                                else if (diasCompromiso.toInt() <= 0)
+                                    errorMessage = "Días de compromiso no puede ser menor a 1"
+                                else {
+                                    scope.launch {
+                                        savePrioridad(
+                                            PrioridadEntity(
+                                                descripcion = descripcion,
+                                                diasCompromiso = diasCompromiso.toInt()
+                                            )
+                                        )
+                                        descripcion = ""
+                                        diasCompromiso = ""
+                                    }
+                                }
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = "Guardar Prioridad"
+                            )
+                            Text("Guardar")
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
-}
+    private suspend fun savePrioridad(prioridad: PrioridadEntity) {
+        prioridadDb.prioridadDao().save(prioridad)
+    }
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    PrioridadesTheme {
-        PrioridadScreen("Android")
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun GreetingPreview() {
+        PrioridadesTheme {
+            PrioridadScreen("Android")
+        }
     }
 }
