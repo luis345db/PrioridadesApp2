b5b7d5535aaab86a11a2e54c11628084e954db8c
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-07 21:53:14 -0600

    Muchos cambios y ajustes para lograr la navegacion

---
+++ app/src/main/java/edu/ucne/prioridades/presentation/navigation/PrioridadNavHost.kt
@@ -0,0 +1,59 @@
+package edu.ucne.prioridades.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.prioridades.presentation.prioridad.PrioridadListScreen
+import edu.ucne.prioridades.presentation.prioridad.PrioridadScreen
+import edu.ucne.prioridades.data.local.database.PrioridadDB
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+@Composable
+fun PrioridadNavHost(
+    navHost: NavHostController,
+    prioridadDb: PrioridadDB
+) {
+    NavHost(
+        navController = navHost,
+        startDestination = Screen.PrioridadList
+    ) {
+        composable<Screen.PrioridadList> {
+            PrioridadListScreen(
+                prioridadDb.prioridadDao().getAll()
+                    .collectAsStateWithLifecycle(
+                        initialValue = emptyList(),
+                        lifecycleOwner = LocalLifecycleOwner.current,
+                        minActiveState = Lifecycle.State.STARTED
+                    ).value,
+                goToPrioridad = {
+                    navHost.navigate(Screen.Prioridad(it))
+                },
+                goToAddPrioridad = {
+                    navHost.navigate(Screen.Prioridad(0))
+                }
+            )
+        }
+
+        composable<Screen.Prioridad> {
+            val args = it.toRoute<Screen.Prioridad>()
+            PrioridadScreen(
+                prioridadId = args.prioridadId,
+                goBack = {
+                    navHost.navigateUp()
+                },
+                prioridadDb = prioridadDb
+            )
+        }
+    }
+}
