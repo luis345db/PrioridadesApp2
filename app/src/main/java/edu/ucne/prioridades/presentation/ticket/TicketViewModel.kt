9068cebeebd2cb136dba35f2a8a8eb21b939443d
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-18 11:19:02 -0600

    actualize el TicketViewModel

--- app/src/main/java/edu/ucne/prioridades/presentation/ticket/TicketViewModel.kt
+++ app/src/main/java/edu/ucne/prioridades/presentation/ticket/TicketViewModel.kt
@@ -3,23 +3,29 @@ package edu.ucne.prioridades.presentation.ticket
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.prioridades.data.local.entities.PrioridadEntity
 import edu.ucne.prioridades.data.local.entities.TicketEntity
+import edu.ucne.prioridades.data.repository.PrioridadRepository
 import edu.ucne.prioridades.data.repository.TicketRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
 class TicketViewModel @Inject constructor(
-    private val ticketRepository: TicketRepository
+    private val ticketRepository: TicketRepository,
+    private val prioridadRepository: PrioridadRepository
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(UiState())
     val uiState = _uiState.asStateFlow()
 
     init {
         getTickets()
+        getPrioridades()
     }
 
     fun save() {
@@ -41,6 +47,8 @@ class TicketViewModel @Inject constructor(
                 ticketId = null,
                 cliente = "",
                 asunto = "",
+                descripcion = "",
+                prioridadId = 0,
                 errorMessage = null
             )
         }
@@ -54,7 +62,10 @@ class TicketViewModel @Inject constructor(
                     it.copy(
                         ticketId = ticket?.ticketId,
                         cliente = ticket?.cliente,
-                        asunto = ticket?.asunto
+                        asunto = ticket?.asunto,
+                        descripcion = ticket?.descripcion,
+                        prioridadId = ticket?.ticketId
+
                     )
                 }
             }
@@ -67,7 +78,7 @@ class TicketViewModel @Inject constructor(
         }
     }
 
-    private fun getTickets() {
+    fun getTickets() {
         viewModelScope.launch {
             ticketRepository.getTickets().collect { tickets ->
                 _uiState.update {
@@ -77,9 +88,25 @@ class TicketViewModel @Inject constructor(
         }
     }
 
+    fun getPrioridades(){
+        viewModelScope.launch {
+            prioridadRepository.getPrioridades().collect(){ prioridades ->
+                _uiState.update {
+                    it.copy(prioridades = prioridades)
+                }
+            }
+        }
+    }
+
     fun onClienteChange(cliente: String) {
         _uiState.update {
             it.copy(cliente = cliente)
+        }
+    }
+
+    fun onDescripcionChange(descripcion: String) {
+        _uiState.update {
+            it.copy(descripcion = descripcion)
         }
     }
 
@@ -95,18 +122,31 @@ class TicketViewModel @Inject constructor(
         }
     }
 
+    fun onPrioridadIdChange(prioridadId: Int) {
+        _uiState.update {
+            it.copy(prioridadId = prioridadId)
+        }
+    }
+
 }
 
 data class UiState(
     val ticketId: Int? = null,
     val cliente: String? = "",
     val asunto: String? = null,
+    val descripcion: String? = null,
     val errorMessage: String? = null,
-    val tickets: List<TicketEntity> = emptyList()
+    val tickets: List<TicketEntity> = emptyList(),
+    val prioridades: List<PrioridadEntity> = emptyList(),
+    val prioridadId: Int? = 0
+
 )
 
 fun UiState.toEntity() = TicketEntity(
     ticketId = ticketId,
     cliente = cliente ?: "",
-    asunto = asunto ?: ""
-)
+    asunto = asunto ?: "",
+    descripcion = descripcion ?: "",
+    prioridadId = prioridadId ?: 0,
+
+    )
