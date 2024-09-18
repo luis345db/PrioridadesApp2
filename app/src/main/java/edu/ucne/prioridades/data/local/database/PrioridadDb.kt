3111b266164eab17c51ff6fee8d83a09d2212984
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-17 18:15:13 -0600

    Agregue Tickets la Base de Datos

--- app/src/main/java/edu/ucne/prioridades/data/local/database/PrioridadDb.kt
+++ app/src/main/java/edu/ucne/prioridades/data/local/database/PrioridadDb.kt
@@ -3,11 +3,14 @@ package edu.ucne.prioridades.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.prioridades.data.local.dao.PrioridadDao
+import edu.ucne.prioridades.data.local.dao.TicketDao
 import edu.ucne.prioridades.data.local.entities.PrioridadEntity
+import edu.ucne.prioridades.data.local.entities.TicketEntity
 
 @Database(
     entities = [
         PrioridadEntity::class
+        ,TicketEntity::class
     ],
     version = 2,
     exportSchema = false
@@ -15,4 +18,5 @@ import edu.ucne.prioridades.data.local.entities.PrioridadEntity
 
 abstract class PrioridadDb : RoomDatabase() {
     abstract fun prioridadDao(): PrioridadDao
+    abstract fun ticketDao(): TicketDao
 }
