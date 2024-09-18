d857c789bdc53dabbdecb4c52413120aa52f7f00
Luis Manuel <luis_duarte@ucne.edu.do>
2024-09-18 11:15:15 -0600

    cambio de la version de la base de datos

--- app/src/main/java/edu/ucne/prioridades/data/local/database/PrioridadDb.kt
+++ app/src/main/java/edu/ucne/prioridades/data/local/database/PrioridadDb.kt
@@ -2,6 +2,7 @@ package edu.ucne.prioridades.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import edu.ucne.prioridades.data.local.dao.PrioridadDao
 import edu.ucne.prioridades.data.local.dao.TicketDao
 import edu.ucne.prioridades.data.local.entities.PrioridadEntity
@@ -12,10 +13,11 @@ import edu.ucne.prioridades.data.local.entities.TicketEntity
         PrioridadEntity::class
         ,TicketEntity::class
     ],
-    version = 2,
+    version = 4,
     exportSchema = false
 )
 
+@TypeConverters(Converters::class)
 abstract class PrioridadDb : RoomDatabase() {
     abstract fun prioridadDao(): PrioridadDao
     abstract fun ticketDao(): TicketDao
